8f6241d0f2b10c46713415582b7cf4606d2bb84b
Nick Barry <itsnickbarry@protonmail.ch>
2025-08-14 20:30:42 -0600

    test constants

---
+++ src/Constants.ts
@@ -0,0 +1,5 @@
+export const MIN_64x64 = -BigInt('0x80000000000000000000000000000000');
+export const MAX_64x64 = BigInt('0x7fffffffffffffffffffffffffffffff');
+export const ONE_64x64 = BigInt('0x10000000000000000');
+export const E_64x64 = BigInt('0x2b7e151628aed1975');
+export const PI_64x64 = BigInt('0x3243f6a8885a2f7a4');
---
+++ test/ABDKMath64x64Constants.ts
@@ -0,0 +1,53 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import {
+  _ABDKMath64x64Constants,
+  _ABDKMath64x64Constants__factory,
+} from '../typechain-types';
+
+import {
+  MIN_64x64,
+  MAX_64x64,
+  ONE_64x64,
+  E_64x64,
+  PI_64x64,
+} from '../src/Constants';
+
+describe('ABDKMath64x64Constants', function () {
+  let instance: _ABDKMath64x64Constants;
+
+  before(async function () {
+    const [deployer] = await ethers.getSigners();
+    instance = await new _ABDKMath64x64Constants__factory(deployer).deploy();
+  });
+
+  describe('MIN_64x64', () => {
+    it('returns correct value', async () => {
+      expect(await instance._MIN_64x64.staticCall()).to.eq(MIN_64x64);
+    });
+  });
+
+  describe('MAX_64x64', () => {
+    it('returns correct value', async () => {
+      expect(await instance._MAX_64x64.staticCall()).to.eq(MAX_64x64);
+    });
+  });
+
+  describe('ONE_64x64', () => {
+    it('returns correct value', async () => {
+      expect(await instance._ONE_64x64.staticCall()).to.eq(ONE_64x64);
+    });
+  });
+
+  describe('E_64x64', () => {
+    it('returns correct value', async () => {
+      expect(await instance._E_64x64.staticCall()).to.eq(E_64x64);
+    });
+  });
+
+  describe('PI_64x64', () => {
+    it('returns correct value', async () => {
+      expect(await instance._PI_64x64.staticCall()).to.eq(PI_64x64);
+    });
+  });
+});
